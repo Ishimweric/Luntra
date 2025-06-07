012ed05ca67007a78d33f2390ad41253908a2054
Ishimwe Eric <ishimwee299@gmail.com>
2025-06-07 22:49:26 +0200

    Added Form Validation and Successful message notification after form submission

--- script.js
+++ script.js
@@ -13,4 +13,17 @@ document.addEventListener('DOMContentLoaded', ()=>{
             container.classList.toggle('active');
         });
     });
+    ///js for form validation(basic)
+    const form= document.querySelector('#contact-form');
+    const successMessage = document.querySelector('.success-message');
+    form.addEventListener('submit', (e)=>{
+        e.preventDefault(); //did this so that a user can't submit if the form is not valid
+        successMessage.textContent='';
+        successMessage.classList.remove('active');
+
+        successMessage.textContent= 'Form submitted successfully!';
+        successMessage.classList.add('active');
+        form.reset();
+
+    });
 });
